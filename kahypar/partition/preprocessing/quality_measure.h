2776b415f0e1b44416a1b5bad92d7af9ef8fa5bf
kittobi1992 <tobias.heuer@gmx.net>
2016-10-05 14:08:38 +0200

    Assert ModularityMeasure-Class

--- kahypar/partition/preprocessing/quality_measure.h
+++ kahypar/partition/preprocessing/quality_measure.h
@@ -11,10 +11,15 @@
 
 #include "kahypar/macros.h"
 #include "kahypar/definitions.h"
+#include "kahypar/datastructure/graph.h"
+#include "kahypar/datastructure/fast_reset_flag_array.h"
 
 namespace kahypar {
 
-    
+using ds::Edge;
+using ds::FastResetFlagArray;
+#define EPS 1e-5    
+
 class QualityMeasure {
 
 friend class Modularity;
@@ -51,6 +56,7 @@ public:
     }
     
     inline void remove(NodeID node, EdgeWeight incidentCommWeight) {
+        ASSERT(node < graph.numNodes(), "NodeID " << node << " doesn't exist!");
         ClusterID cid = graph.clusterID(node);
         
         in[cid] -= 2.0L*incidentCommWeight + graph.selfloopWeight(node);
@@ -60,20 +66,45 @@ public:
     }
     
     inline void insert(NodeID node, ClusterID new_cid, EdgeWeight incidentCommWeight) {
+        ASSERT(node < graph.numNodes(), "NodeID " << node << " doesn't exist!");
+        ASSERT(graph.clusterID(node) == -1, "Node " << node << " isn't a isolated node!");
         
         in[new_cid] += 2.0L*incidentCommWeight + graph.selfloopWeight(node);
         tot[new_cid] += graph.weightedDegree(node);
         
         graph.setClusterID(node,new_cid);
+        
+        ASSERT([&]() {
+           EdgeWeight q = quality();
+           return q < std::numeric_limits<EdgeWeight>::max();
+        }(), "");
     }
     
     inline EdgeWeight gain(NodeID node, ClusterID cid, EdgeWeight incidentCommWeight) {
+        ASSERT(node < graph.numNodes(), "NodeID " << node << " doesn't exist!");
+        ASSERT(graph.clusterID(node) == -1, "Node " << node << " isn't a isolated node!");
         
         EdgeWeight totc = tot[cid];
         EdgeWeight m2 = graph.totalWeight();
         EdgeWeight w_degree = graph.weightedDegree(node);
         
-        return (incidentCommWeight - totc*w_degree/m2);
+        EdgeWeight gain = incidentCommWeight - totc*w_degree/m2;
+        
+        ASSERT([&]() {
+            EdgeWeight modularity_before = modularity();
+            insert(node,cid,incidentCommWeight);
+            EdgeWeight modularity_after = modularity();
+            remove(node,incidentCommWeight);
+            EdgeWeight real_gain = modularity_after - modularity_before;
+            if(std::abs(2.0L*gain/m2 - real_gain) > EPS) {
+                LOGVAR(real_gain);
+                LOGVAR(2.0L*gain/m2);
+                return false;
+            }
+            return true;
+        }(), "Gain calculation failed!");
+        
+        return gain;
     }
     
     
@@ -82,20 +113,49 @@ public:
         EdgeWeight m2 = graph.totalWeight();
         
         for(NodeID node : graph.nodes()) {
-            if(tot[node] > 0.0L) {
+            if(tot[node] > EPS) {
                 q += in[node] - (tot[node]*tot[node])/m2;
             }
         }
         
         q /= m2;
         
+        ASSERT(std::abs(q-modularity()) < EPS, "Calculated modularity (q=" << q << ") is not equal with the real modularity (modularity=" << modularity() << ")!");
+        
         return q;
     }
     
 private:
-    FRIEND_TEST(AModularityMeasure,IsCorrectInitialized);    FRIEND_TEST(AModularityMeasure,RemoveNodeFromCommunity);
+    FRIEND_TEST(AModularityMeasure,IsCorrectInitialized);    
+    FRIEND_TEST(AModularityMeasure,RemoveNodeFromCommunity);
     FRIEND_TEST(AModularityMeasure,InsertNodeInCommunity);
     FRIEND_TEST(AModularityMeasure,RemoveNodeFromCommunityWithMoreThanOneNode);
+    
+    EdgeWeight modularity() {
+        EdgeWeight q = 0.0L;
+        EdgeWeight m2 = graph.totalWeight();
+        
+        FastResetFlagArray<> vis(graph.numNodes());
+        
+        for(NodeID u : graph.nodes()) {
+            for(Edge edge : graph.adjacentNodes(u)) {
+                NodeID v = edge.targetNode;
+                vis.set(v,true);
+                if(graph.clusterID(u) == graph.clusterID(v)) {
+                    q += edge.weight - (graph.weightedDegree(u)*graph.weightedDegree(v))/m2;
+                }
+            }
+            for(NodeID v : graph.nodes()) {
+                if(graph.clusterID(u) == graph.clusterID(v) && !vis[v]) {
+                    q -= (graph.weightedDegree(u)*graph.weightedDegree(v))/m2;
+                }
+            }
+            vis.reset();
+        }
+        
+        q /= m2;
+        return q;
+    }
     
     using QualityMeasure::graph;
     std::vector<EdgeWeight> in;
